4b0231863b58d4b8dc64da758d85dbd67a99fd88
unknown <alexandre.gcms@gmail.com>
2024-10-27 21:27:03 +0000

    add frontend connec

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
-const app = express();
-const server = http.createServer(app);
-
-const io = socketIo(server, {
-  cors: {
-    origin: [
-      'https://quick-connect-client.vercel.app', 
-    ],
-    methods: ['GET', 'POST'], 
-  },
-});
+const app = require('express')();
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173'}})
 
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3001; 
 
+io.on('connection',socket => {
+    console.log('Usuario conectado', socket.id)
 
-io.on('connection', (socket) => {
-  console.log('User connected', socket.id);
+    socket.on('disconnect', reason => {
+        console.log('Usuario desconectado', socket.id)
+    })
 
-  socket.on('set_username', (username) => {
-    socket.data.username = username;
-  });
+    socket.on('set_username', username => {
+        socket.data.username = username        
+    })
 
-  socket.on('message', (text) => {
-    io.emit('receive_message', {
-      text,
-      authorId: socket.id,
-      author: socket.data.username || 'Anonymous', // Se o nome de usuário não for definido
-    });
-  });
-
-  socket.on('disconnect', (reason) => {
-    console.log('User disconnected', socket.id);
-  });
-});
+    socket.on('message', text => {
+        io.emit('receive_message', {
+            text,
+            authorId: socket.id,
+            author: socket.data.username
+        })
+    })
+})
 
 server.listen(PORT, '0.0.0.0', () => {
     console.log('aaaaaaa')
