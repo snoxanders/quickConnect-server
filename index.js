b2ee42e4b82bcd4c6c43eace44931ddab7f99ac3
unknown <alexandre.gcms@gmail.com>
2024-10-27 21:12:22 +0000

    add frontend connec

--- index.js
+++ index.js
@@ -5,31 +5,26 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 
-// Configuração do Socket.IO com CORS
 const io = socketIo(server, {
   cors: {
     origin: [
-      'https://quick-connect-client.vercel.app', // URL do cliente na Vercel
-      // Você pode adicionar outras origens aqui se necessário
+      'https://quick-connect-client.vercel.app', 
     ],
-    methods: ['GET', 'POST'], // Métodos permitidos
-    credentials: true, // Permitir cookies e cabeçalhos de autorização
+    methods: ['GET', 'POST'], 
   },
 });
 
-// Defina uma porta padrão, se PORT não estiver definido
-const PORT = process.env.PORT || 3000; // Altere 3000 para a porta desejada
 
-// Evento quando um usuário se conecta
+const PORT = process.env.PORT || 3000; 
+
+
 io.on('connection', (socket) => {
   console.log('User connected', socket.id);
 
-  // Evento para definir o nome de usuário
   socket.on('set_username', (username) => {
     socket.data.username = username;
   });
 
-  // Evento para receber mensagens
   socket.on('message', (text) => {
     io.emit('receive_message', {
       text,
@@ -38,13 +33,12 @@ io.on('connection', (socket) => {
     });
   });
 
-  // Evento quando um usuário se desconecta
   socket.on('disconnect', (reason) => {
     console.log('User disconnected', socket.id);
   });
 });
 
-// Inicie o servidor
 server.listen(PORT, '0.0.0.0', () => {
+    console.log('aaaaaaa')
   console.log(`Server running on port ${PORT}`);
 });
