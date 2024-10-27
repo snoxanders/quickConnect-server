2da49b62ef51a217514cf755698a8ecad7e94143
unknown <alexandre.gcms@gmail.com>
2024-10-27 21:53:43 +0000

    add frontend connec

--- index.js
+++ index.js
@@ -1,52 +1,37 @@
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
+const app = require('express')();
+const server = require('http').createServer(app)
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://quick-connect-client.vercel.app', // URL do seu cliente
+    origin: 'https://quick-connect-client.vercel.app',
     methods: ['GET', 'POST'],
     credentials: true,
   },
 });
 
-// Definindo a porta do servidor
-const PORT = process.env.PORT || 3001;
 
-// Mensagem de log para confirmar a porta
-if (!process.env.PORT) {
-  console.warn('Usando a porta padrão 3001. Configure a variável de ambiente PORT.');
-}
+const PORT = process.env.PORT || 3001; 
 
-// Rota raiz para verificação rápida do status do servidor
-app.get('/', (req, res) => {
-  res.send('Servidor Socket.IO está ativo');
-});
+io.on('connection',socket => {
+    console.log('Usuario conectado', socket.id)
 
-// Configuração dos eventos do Socket.IO
-io.on('connection', (socket) => {
-  console.log(`Usuário conectado: ${socket.id}`);
+    socket.on('disconnect', reason => {
+        console.log('Usuario desconectado', socket.id)
+    })
 
-  socket.on('disconnect', (reason) => {
-    console.log(`Usuário desconectado: ${socket.id}, motivo: ${reason}`);
-  });
+    socket.on('set_username', username => {
+        socket.data.username = username        
+    })
 
-  socket.on('set_username', (username) => {
-    socket.data.username = username;
-    console.log(`Usuário ${socket.id} definiu o nome como: ${username}`);
-  });
+    socket.on('message', text => {
+        io.emit('receive_message', {
+            text,
+            authorId: socket.id,
+            author: socket.data.username
+        })
+    })
+})
 
-  socket.on('message', (text) => {
-    const messageData = {
-      text,
-      authorId: socket.id,
-      author: socket.data.username || 'Anônimo', // Nome de autor padrão
-    };
-    io.emit('receive_message', messageData);
-    console.log('Mensagem enviada:', messageData);
-  });
-});
-
-// Iniciando o servidor
 server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log('aaaaaaa')
+  console.log(`Server running on port ${PORT}`);
 });
