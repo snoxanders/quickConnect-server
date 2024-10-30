11f896044b2dd4e8e3529d4dd6caf59967b85321
unknown <alexandre.gcms@gmail.com>
2024-10-30 23:31:13 +0000

    add frontend connec

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const app = require('express')();
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://quick-connect-client.vercel.app',
-    methods: ['GET', 'POST'],                         
-    credentials: true                                  
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true
   },
+  
 });
 
 
