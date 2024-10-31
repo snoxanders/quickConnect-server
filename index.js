0dbd33a1289ec15b6ceab37d627f8ac74f40ebcc
unknown <alexandre.gcms@gmail.com>
2024-10-31 00:15:39 +0000

    add frontend connec

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
 const app = require('express')();
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {
-  cors: {
-    origin: 'https://quick-connect-client.vercel.app',
-    methods: ['GET', 'POST'],                         
-    credentials: true                                  
-  },
-});
+const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173'}})
 
 
 const PORT = process.env.PORT || 3001; 
-
-app.get('/', (req, res) => {
-  res.send('Servidor rodando!');
-});
 
 io.on('connection',socket => {
     console.log('Usuario conectado', socket.id)
 
     socket.on('disconnect', reason => {
-        console.log('Usuario desconectado!', socket.id)
+        console.log('Usuario desconectado', socket.id)
     })
 
     socket.on('set_username', username => {
