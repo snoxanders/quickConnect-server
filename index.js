ed1d7fa3c2a6c9bbcdf444b66282e494eb11583c
unknown <alexandre.gcms@gmail.com>
2024-11-09 21:41:18 +0000

    weae

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {cors: {origin: 'https://quick-connect-client-gfoom72zk-snoxanders-projects.vercel.app/'}})
+const io = require('socket.io')(server, {cors: {origin: 'https://quick-connect-client-gfoom72zk-snoxanders-projects.vercel.app'}})
 
 
 const PORT = process.env.PORT || 3001; 
